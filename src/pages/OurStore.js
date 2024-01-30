ea68fb8ea9f862833e3ff7085bd01432d9b0a46e
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2024-01-30 16:32:14 +0300

    feat:filtersearch

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -196,7 +196,25 @@ const OurStore = () => {
                 </div>
               </div>
             </div>
-            <div className='col-9'></div>
+            <div className='col-9'>
+              <div className='filter-sort-grid'>
+                <div className='d-flex  justify-content-between align-items-center'>
+                  <div className='d-flex align-items-center gap-10'>
+                    <p className='mb-0 d-block'>Sort By:</p>
+                    <select name="" className="form-control form-select" id="">
+                    <option value="manual">Featured</option>
+                    <option value="best-selling" selected="selected">Best selling</option>
+                    <option value="title-ascending">Alphabetically, A-Z</option>
+                    <option value="title-descending">Alphabetically, Z-A</option>
+                    <option value="price-ascending">Price, low to high</option>
+                    <option value="price-descending">Price, high to low</option>
+                    <option value="created-ascending">Date, old to new</option>
+                    <option value="created-descending">Date, new to old</option></select>
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>

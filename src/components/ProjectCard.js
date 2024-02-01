866d8b0d714abd4793f66a402bd8ca79ed9d9368
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2024-02-01 12:14:11 +0300

    Done with product image section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import ReactStars from "react-rating-stars-component";
 import watchImg from '../assets/images/watch.jpg';
 import watchImg1 from '../assets/images/watch-01.jpg';
@@ -9,9 +9,13 @@ import addCartImg from '../assets/images/add-cart.svg';
 import wishListImg from '../assets/images/wish.svg';
 
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
+    const location = useLocation();
+    const { grid } = props;
+
   return (
-    <div className='col-3'>
+    <>
+        <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
         <Link className='product-card position-relative'>
             <div className='wishlist-icon position-absolute'>
                 <Link>
@@ -34,6 +38,7 @@ const ProjectCard = () => {
                     edit={false}
                     activeColor="#ffd700"
                 />
+                <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly...</p>
                 <p className="price">$24.99</p>
             </div>
             <div className='action-bar position-absolute'>
@@ -51,6 +56,48 @@ const ProjectCard = () => {
             </div>
         </Link>
     </div>
+    <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
+        <Link className='product-card position-relative'>
+            <div className='wishlist-icon position-absolute'>
+                <Link>
+                    <img src={wishListImg} alt="wish list" />
+                </Link>
+            </div>
+            <div className='product-image'>
+                <img className='img-fluid' src={watchImg1} alt='product'/>
+                <img className='img-fluid' src={watchImg} alt='product'/>
+            </div>
+            <div className='product-details'>
+                <h6 className='brand'>Havels</h6>
+                <h5 className='product-title'>
+                    Kids headphones bulk 10 pack multi colored for students
+                </h5>
+                <ReactStars
+                    count={5}
+                    size={24}
+                    value={4}
+                    edit={false}
+                    activeColor="#ffd700"
+                />
+                <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly...</p>
+                <p className="price">$24.99</p>
+            </div>
+            <div className='action-bar position-absolute'>
+                <div className='d-flex flex-column gap-15'>
+                    <Link to='/'>
+                        <img src={prodCompareImg} alt='compare'/>
+                    </Link>
+                    <Link to='/'>
+                        <img src={viewImg} alt='view'/>
+                    </Link>
+                    <Link to='/'>
+                        <img src={addCartImg} alt='add cart'/>
+                    </Link>
+                </div>
+            </div>
+        </Link>
+    </div>
+    </>
   )
 }
 
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BreadCrumb from '../components/BreadCrumb';
+import ProductCard from '../components/ProjectCard';
 import Meta from '../components/Meta';
 import ReactStars from "react-rating-stars-component";
-import watchImg from '../assets/images/watch.jpg'
+import watchImg from '../assets/images/watch.jpg';
+import gridImg1 from '../assets/images/gr.svg';
+import gridImg2 from '../assets/images/gr2.svg';
+import gridImg3 from '../assets/images/gr3.svg';
+import gridImg4 from '../assets/images/gr4.svg';
 
 const OurStore = () => {
+    const [grid, setGrid] = useState(4);
+    
+
   return (
     <>
       <Meta title={'Our Store'}/>
@@ -197,10 +205,10 @@ const OurStore = () => {
               </div>
             </div>
             <div className='col-9'>
-              <div className='filter-sort-grid'>
+              <div className='filter-sort-grid mb-4'>
                 <div className='d-flex  justify-content-between align-items-center'>
                   <div className='d-flex align-items-center gap-10'>
-                    <p className='mb-0 d-block'>Sort By:</p>
+                    <p className='mb-0 d-block' style={{ width: "100px" }}>Sort By:</p>
                     <select name="" className="form-control form-select" id="">
                     <option value="manual">Featured</option>
                     <option value="best-selling" selected="selected">Best selling</option>
@@ -209,9 +217,51 @@ const OurStore = () => {
                     <option value="price-ascending">Price, low to high</option>
                     <option value="price-descending">Price, high to low</option>
                     <option value="created-ascending">Date, old to new</option>
-                    <option value="created-descending">Date, new to old</option></select>
+                    <option value="created-descending">Date, new to old</option>
+                    </select>
                   </div>
-                  <div></div>
+                  <div className='d-flex align-items-center gap-10'>
+                    <p className='totalproducts mb-0'>21 Products</p>
+                    <div className='d-flex align-items-center gap-10 grid'>
+                    <img
+                      onClick={() => {
+                        setGrid(3);
+                      }}
+                        className='d-block img-fluid'
+                        src={gridImg4}
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(4);
+                        }}
+                        className='d-block img-fluid'
+                        src={gridImg3}
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(6);
+                        }}
+                        className='d-block img-fluid'
+                        src={gridImg2}
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(12);
+                        }}
+                        className='d-block img-fluid'
+                        src={gridImg1}
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='products-list pb-5'>
+                <div className='d-flex gap-10 flex-wrap'>
+                <ProductCard grid={grid} />
                 </div>
               </div>
             </div>

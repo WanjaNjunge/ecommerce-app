31b3dc7f3f5c8c3f6292d703b0003d73b0ea8226
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2024-02-29 22:49:12 +0300

    Redux user registration set up

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { authService } from './userService';
+
+export const registerUser = createAsyncThunk("auth/register", async (userData, thunkAPI)=>{
+    try{
+        return await authService.register(userData)
+    } catch(error) {
+        return thunkAPI.rejectWithValue(error)
+    }
+})
+
+const initialState = {
+    user:"",
+    isError:false,
+    isSuccess:false,
+    isLoading:false,
+    message:""
+}
+
+export const authSlice = createSlice({
+    name:"auth",
+    initialState:initialState,
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder.addCase(registerUser.pending, (state)=>{
+            state.isLoading=true;
+        }).addCase(registerUser.fulfilled, (state, action)=> {
+            state.isError=false;
+            state.isSuccess=true;
+            state.isLoading=false;
+            
+        }).addCase(registerUser.rejected, (state, action)=>{
+            state.isError=true;
+            state.isSuccess=false;
+            state.isLoading=false;
+            state.message=action.error
+        })
+    }
+})
+
+export default authSlice.reducer;
